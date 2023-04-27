8e0cf584d1202e8a01d76b001e9f122b4dbb5fba
laura-barrera-valdes <laurabv99@gmail.com>
2023-04-27 12:03:17 -0500

    feat: css

--- src/components/MyProfile/MyProfile.ts
+++ src/components/MyProfile/MyProfile.ts
@@ -1,68 +1,79 @@
+import { loadCss } from "../../utils/styles";
+import styles from "./MyProfile.css";
+
 export enum myprofileAttribute {
-    "mybackground" = "mybackground",
-    "myprofileimage" = "myprofileimage",
-    "myname" = "myname",
-    "mygender" = "mygender",
-    "myage" = "myage",
-    "mydescription" = "mydescription",
-    "mybreed" = "mybreed",
-    "myactivity" = "myactivity",
-    "mylocation" = "mylocation"
+  "mybackground" = "mybackground",
+  "myprofileimage" = "myprofileimage",
+  "myname" = "myname",
+  "mygender" = "mygender",
+  "myage" = "myage",
+  "mydescription" = "mydescription",
+  "mybreed" = "mybreed",
+  "myactivity" = "myactivity",
+  "mylocation" = "mylocation",
 }
 
-class MyprofileCard extends HTMLElement{
-    mybackground?:string;
-    myprofileimage?: string;
-    myname?: string;
-    mygender?: string;
-    myage?: string;
-    mydescription?:string;
-    mybreed?: string;
-    myactivity?: string;
-    mylocation?: string;
-    
+class MyprofileCard extends HTMLElement {
+  mybackground?: string;
+  myprofileimage?: string;
+  myname?: string;
+  mygender?: string;
+  myage?: string;
+  mydescription?: string;
+  mybreed?: string;
+  myactivity?: string;
+  mylocation?: string;
 
-    static get observedAttributes(){
-        const myattrs: Record<myprofileAttribute, null> = {
-            mybackground: null,
-            myprofileimage: null,
-            myname: null,
-            mygender: null,
-            myage: null,
-            mydescription: null,
-            mybreed: null,
-            myactivity: null,
-            mylocation: null,
-        };
-        return Object.keys(myattrs);
-    }
+  static get observedAttributes() {
+    const myattrs: Record<myprofileAttribute, null> = {
+      mybackground: null,
+      myprofileimage: null,
+      myname: null,
+      mygender: null,
+      myage: null,
+      mydescription: null,
+      mybreed: null,
+      myactivity: null,
+      mylocation: null,
+    };
+    return Object.keys(myattrs);
+  }
 
-    constructor(){
-        super();
-        this.attachShadow({mode: "open"});
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-    connectedCallback(){
-        this.render();
-    }
+  connectedCallback() {
+    this.render();
+  }
 
-    attributeChangedCallback(
-        propName: myprofileAttribute,
-        _: string | undefined,
-        newValue: string | undefined
-){
-    
-   this[propName] = newValue;
+  attributeChangedCallback(
+    propName: myprofileAttribute,
+    _: string | undefined,
+    newValue: string | undefined
+  ) {
+    this[propName] = newValue;
 
     this.render();
-}
+  }
 
-render(){
-    if (this.shadowRoot){
-        this.shadowRoot.innerHTML = `
+  render() {
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = `
         <section class="myprofilecard">
-            <img src="${this.mybackground}">
-            <img src="${this.myprofileimage}">
+            <article class="">
+                <img src="${this.mybackground}">
+            </article>
+            <article class="">
+                <img class="imgmyprof" src="${this.myprofileimage}">
+            </article>
+            <article class="">
+                <p class="mydescription">${this.myname}</p>
+                <p class="mydescription">${this.mydescription}</p>
+            </article>
+
+
             <article class="mygender">
                 <img class="iconmyprof" src="./assets/icon/my_profile/gender.png">
                  <p>${this.mygender}</p>
@@ -72,16 +83,8 @@ render(){
                 <img class="iconmyprof" src="./assets/icon/my_profile/age.png">
                 <p>${this.myage}</p>
             </article>
-            
-            
+        
                 <section class="myinfo">
-                    <p class="mydescription">${this.mydescription}</p>
-
-                    <article class="mybreed">
-                        <img class="iconmyprof" src="./assets/icon/my_profile/breed.png">
-                        <p>${this.mybreed}</p>
-                    </article>
-
                     <article class="myactivity">
                         <img class="iconmyprof" src="./assets/icon/my_profile/activity.png">
                         <p>${this.myactivity}</p>
@@ -96,9 +99,9 @@ render(){
                 <button class="editprofile">Edit Profile</button>
         </section>
         `;
+      loadCss(this, styles);
     }
-}
-
+  }
 }
 
 customElements.define("myprofile-card", MyprofileCard);
