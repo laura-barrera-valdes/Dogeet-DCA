e578756fcb2c33d3b3d45d46b5de27b3a8dbfed0
laura-barrera-valdes <laurabv99@gmail.com>
2023-04-27 12:12:33 -0500

    feat: register

---
+++ src/components/RegisterCard/RegisterCard.ts
@@ -0,0 +1,11 @@
+export enum registerAttribute {
+  "login" = "login",
+  "email" = "email",
+  "password" = "password",
+}
+
+class Myregister extends HTMLElement {
+  login?: string;
+  email?: string;
+  password?: string;
+}
