134e784c32b043d913e95a4def7d33bec1834ce6
valenarangot <valentina.arango.tenorio@gmail.com>
2023-04-30 18:40:56 -0500

    feat: input

---
+++ src/components/Input/Input.ts
@@ -0,0 +1,47 @@
+export enum inputAttribute {
+  "placeholder" = "placeholder",
+}
+
+class Input extends HTMLElement {
+  placeholder?: string;
+
+
+  static get observedAttributes(){
+    const registerattrs: Record<inputAttribute, null> ={
+      placeholder: null,   
+    };
+    return Object.keys(registerattrs);
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  attributeChangedCallback(
+    propName: inputAttribute,
+    _: string | undefined,
+    newValue: string | undefined
+  ) {
+    this[propName] = newValue;
+
+    this.render();
+  }
+
+  render(){
+    if(this.shadowRoot){
+      this.shadowRoot.innerHTML = `
+        <input placeholder="${this.placeholder}"></input>
+      `
+    }
+  }
+
+
+}
+
+customElements.define("input-component", Input);
+export default Input;
