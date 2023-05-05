0528cee536c9c21ca43edb9d1738512be38beb96
laura-barrera-valdes <laurabv99@gmail.com>
2023-05-05 14:05:22 -0500

    feat: css

--- src/components/Button/Button.ts
+++ src/components/Button/Button.ts
@@ -1,48 +1,48 @@
+import { loadCss } from "../../utils/styles";
+import styles from "./Button.css";
+
 export enum buttonAttribute {
-    "button" = "button",
+  "button" = "button",
+}
+
+class Button extends HTMLElement {
+  button?: string;
+
+  static get observedAttributes() {
+    const buttonattrs: Record<buttonAttribute, null> = {
+      button: null,
+    };
+    return Object.keys(buttonattrs);
   }
-  
-  class Button extends HTMLElement {
-    button?: string;
-  
-  
-    static get observedAttributes(){
-      const buttonattrs: Record<buttonAttribute, null> ={
-        button: null,   
-      };
-      return Object.keys(buttonattrs);
-    }
-  
-    constructor() {
-      super();
-      this.attachShadow({ mode: "open" });
-    }
-  
-    connectedCallback() {
-      this.render();
-    }
-  
-    attributeChangedCallback(
-      propName: buttonAttribute,
-      _: string | undefined,
-      newValue: string | undefined
-    ) {
-      this[propName] = newValue;
-  
-      this.render();
-    }
-  
-    render(){
-      if(this.shadowRoot){
-        this.shadowRoot.innerHTML = `
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  attributeChangedCallback(
+    propName: buttonAttribute,
+    _: string | undefined,
+    newValue: string | undefined
+  ) {
+    this[propName] = newValue;
+
+    this.render();
+  }
+
+  render() {
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = `
         <button class="button-component">${this.button}</button>
-        `
-      }
+        `;
+      loadCss(this, styles);
     }
-  
-  
   }
-  
-  customElements.define("button-component", Button);
-  export default Button;
-  
+}
+
+customElements.define("button-component", Button);
+export default Button;
